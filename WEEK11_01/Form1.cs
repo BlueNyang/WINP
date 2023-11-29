94119fef30662efed454038cf746af80feba0859
BlueNyang <xpsj20@gmail.com>
2023-11-29 18:58:54 +0900

    WEKK11

--- WEEK11_01/Form1.cs
+++ WEEK11_01/Form1.cs
@@ -14,21 +14,24 @@ namespace WEEK11_01
 {
     public partial class Form1 : Form
     {
+
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
             Paint += new PaintEventHandler(Painting);
             Resize += new EventHandler(FormResize);
+            g = CreateGraphics();
         }
 
         private void FormResize(object sender, EventArgs e)
         {
-            Refresh();
+            Invalidate();
         }
 
         private void Painting(object sender, PaintEventArgs e)
         {
-            gradient();
+            DrawRainbowStar();
         }
         private void painting()
         {
@@ -41,17 +44,17 @@ namespace WEEK11_01
 
         private void wkulogo()
         {
-            Graphics g = CreateGraphics();
-
-            string filename = @"\원광대학교.jpg";
+            string filename = @"\원광대로고.png";
             if (!File.Exists(Environment.CurrentDirectory + filename))
                 File.Copy($@"C:\Users\xpsj2\응애\4학기\Win{filename}", Environment.CurrentDirectory + filename);
-            Image img = new Bitmap("원광대학교.jpg");
+            Image img = new Bitmap("원광대로고.png");
             TextureBrush b = new TextureBrush(img);
+            g.FillRectangle(b, ClientRectangle);
+            img.Dispose();
+            b.Dispose();
         }
         private void gradient()
         {
-            Graphics g = CreateGraphics();
             List<Color> colors = new List<Color>()
             {
                 Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Orchid, Color.Violet
@@ -69,8 +72,6 @@ namespace WEEK11_01
 
         private void gradient2()
         {
-            Graphics g = CreateGraphics();
-
             Point[] pts = {
                 new Point(ClientRectangle.Width / 2, 0),
                 new Point(0, ClientRectangle.Height),
@@ -78,20 +79,19 @@ namespace WEEK11_01
             };
 
             PathGradientBrush b = new PathGradientBrush(pts);
-            ColorBlend cb = new ColorBlend();
             Color[] colors =
             {
                 Color.Red, Color.Blue, Color.Green
             };
+
             b.SurroundColors = colors;
-            b.CenterColor = Color.White
-                ;
+            b.CenterColor = Color.White;
+
             g.FillRectangle(b, ClientRectangle);
             b.Dispose();
         }
         private void DrawLine()
         {
-            Graphics g = CreateGraphics();
             Pen p = new Pen(Color.Black, 10);
             p.Width = 7;
             Point startPoint = new Point(45, 45);
@@ -103,8 +103,6 @@ namespace WEEK11_01
 
         private void DrawLines()
         {
-            Graphics g = CreateGraphics();
-
             Point[] pts =
             {
                 new Point(40, 40),                new Point(180, 40),
@@ -124,8 +122,6 @@ namespace WEEK11_01
 
         private void DrawRectangle()
         {
-            Graphics g = CreateGraphics();
-
             Rectangle r = new Rectangle(50, 50, 150, 100);
             g.FillRectangle(Brushes.Lime, r);
             g.DrawRectangle(new Pen(Color.Blue), r);
@@ -134,8 +130,6 @@ namespace WEEK11_01
 
         private void DrawRectangles()
         {
-            Graphics g = CreateGraphics();
-
             Rectangle[] rects =
             {
                 new Rectangle(40, 40, 40, 100),
@@ -145,5 +139,151 @@ namespace WEEK11_01
             g.FillRectangles(Brushes.Blue, rects);
             g.DrawRectangles(Pens.Red, rects);
         }
+
+        private void DrawingEllipse()
+        {
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillEllipse(Brushes.Cyan, r);
+            g.DrawEllipse(Pens.Black, r);
+        }
+
+        private void DrawArc()
+        {
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.DrawArc(Pens.Red, r, 45, 270);
+        }
+
+        private void DrawPie()
+        {
+            Rectangle r = new Rectangle(50, 50, 150, 100);
+            g.FillPie(Brushes.LightGreen, r, 45, 270);
+            g.DrawPie(Pens.DarkGreen, r, 45, 270);
+        }
+        private void DrawRainbowPizza()
+        {
+            Rectangle r = new Rectangle(100, 100, 100, 100);
+            int degree = 360;
+            Color[] color = { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Indigo, Color.Purple };
+            for(int i =0; i<7; i++)
+            {
+                g.FillPie(new SolidBrush(color[i]), r, degree, -(360 / 8));
+                g.DrawPie(Pens.Black, r, degree, -(360 / 8));
+                degree -= 45;
+            }
+        }
+
+        private void DrawStar()
+        {
+            Point[] pts =
+            {
+                new Point(110,40), new Point(125,91),
+                new Point(180, 91), new Point(135,123),
+                new Point(152,172), new Point(110,141),
+                new Point(66,172), new Point(82,122),
+                new Point(40,91), new Point(95,91)
+            };
+            g.FillPolygon(Brushes.Pink, pts);
+            g.DrawPolygon(Pens.Purple, pts);
+        }
+
+        private void DrawRainbowStar()
+        {
+            Point[] pts =
+            {
+                new Point(110,40), new Point(125,91),
+                new Point(180, 91), new Point(135,123),
+                new Point(152,172), new Point(110,141),
+                new Point(66,172), new Point(82,122),
+                new Point(40,91), new Point(95,91)
+            };
+            Point Center = new Point(0, 0);
+            for (int i = 0; i < pts.Length/2; i++)
+            {
+                Center.X += pts[i*2].X;
+                Center.Y += pts[i*2].Y;
+            }
+            Center.X = Center.X / (pts.Length/2);
+            Center.Y = Center.Y / (pts.Length/2);
+            Color[] Colors = { Color.Red, Color.Yellow, Color.Green, Color.Blue, Color.Purple };
+            int index = 0;
+
+            for(int i = 0; i < 5; i++)
+            {
+                Point[] temp = { pts[index], pts[(index + 1) % pts.Length], pts[(index + 2) % pts.Length], Center };
+                g.FillPolygon(new SolidBrush(Colors[i]), temp);
+                g.DrawPolygon(Pens.Black, temp);
+                index += 2;
+            }
+        }
+
+        private void DrawCurves()
+        {
+            Point[] point = { new Point(40, 100), new Point(50, 60), new Point(60, 50), new Point(70, 60),
+new Point(80, 100), new Point(90, 140), new Point(100, 150), new Point(110, 140),
+new Point(120, 100), new Point(130, 60), new Point(140, 50), new Point(150, 60),
+new Point(160, 100), new Point(170, 140), new Point(180, 150), new Point(190, 140),
+new Point(200, 100)};
+
+            g.DrawCurve(Pens.Red, point);
+        }
+
+        private void DrawClosedCurve()
+        {
+            Point[] pts = {
+new Point(115, 30), new Point(140, 90),
+new Point(200, 115), new Point(140, 140),
+new Point(115, 200), new Point(90, 140),
+new Point(30, 115), new Point(90, 90)
+};
+            g.FillClosedCurve(Brushes.Yellow, pts);
+            g.DrawClosedCurve(Pens.Red, pts);
+        }
+
+        private void DrawBezier()
+        {
+            g.DrawBezier(Pens.Magenta, new Point(100, 50), new Point(0, 100), new Point(200, 100), new Point(100, 150));
+        }
+
+        private void DrawBezierCurve()
+        {
+            Point[] pts = {
+                new Point(30, 30),
+                new Point(60, 30),new Point(30, 60),
+                new Point(60, 60),
+                new Point(90, 60), new Point(60, 90),
+                new Point(90, 90),
+                new Point(120, 90), new Point(90, 120),
+                new Point(120, 120),
+                new Point(150, 120), new Point(120, 150),
+                new Point(150, 150)};
+            g.DrawBeziers(Pens.DeepPink, pts);
+        }
+
+        private void DrawString1()
+        {
+            string s = "Hello World";
+            Font f = new Font("Tahoma", 15);
+            g.DrawString(s, f, Brushes.Black, 10, 10);
+            f.Dispose();
+        }
+
+        private void DrawString2()
+        {
+            string s = "This string is long enough to wrap.";
+            s += " With a 250pxwidth rectangle, ";
+            s += "it requires six lines to display the string in its entirety.";
+            Font f = new Font("Tahoma", 15);
+            Rectangle r = new Rectangle(10, 10, 250, 150);
+            g.DrawRectangle(Pens.Black, r);
+            g.DrawString(s, f, Brushes.Black, r);
+            f.Dispose();
+        }
+
+        private void DrawString3()
+        {
+
+        }
+
+
     }
 }
